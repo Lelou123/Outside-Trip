5b4f2e9b91da0060a2a0fb7cf8e589f9286d43b1
vitoriaarakaki <vitoriaakemiarakaki@gmail.com>
2022-04-23 16:33:21 -0300

    Vitoria AdicionaClienteClasse

--- src/main/java/Trip/Cliente/Cliente.java
+++ src/main/java/Trip/Cliente/Cliente.java
@@ -6,7 +6,6 @@ public class Cliente {
 	private Integer Int;
 	private String Name;
 	private String Senha;
-	private String Usuario;
 	private String Email;
 	private String Documento;
 	private Date DataNascimento;
