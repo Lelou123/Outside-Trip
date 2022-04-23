e332b20698925aeaa9f3625e07de8b713e505d48
Lelou123 <murillosama1@hotmail.com>
2022-04-23 15:40:47 -0300

    Murillo ClienteClasse

--- src/main/java/Trip/Cliente/Cliente.java
+++ src/main/java/Trip/Cliente/Cliente.java
@@ -1,8 +1,15 @@
 package Trip.Cliente;
 
+import java.util.Date;
+
 public class Cliente {
-	private int Int;
+	private Integer Int;
 	private String Name;
 	private String UserName;
 	private String Senha;
+	private String Email;
+	private String Documento;
+	private Date DataNascimento;
+	private String Telefone;
+	
 }
